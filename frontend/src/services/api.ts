ca56594b1d6ba59d3095a7507f6c429ee7983245
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-06-25 18:39:38 -0300

    feat: update api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,4 @@
 import axios, { AxiosInstance } from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export const api: AxiosInstance = axios.create({
   baseURL: "https://blockBuster.local/api/v1/",
